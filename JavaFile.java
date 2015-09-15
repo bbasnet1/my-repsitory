3f1f2f99896aa870f3c5bda06893f1184039e013
bbasnet1 <basnetbishnu@live.com>
2015-09-15 09:36:14 -0700

    Update JavaFile.java

--- JavaFile.java
+++ JavaFile.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
+public class Practice
+{
+	public static void main(String[] args)
+	{
+		Scanner in = new Scanner(System.in);
 
+		System.out.println("\"Please enter the price: \"");
+		double packPrice = in.nextDouble();
+
+		System.out.print("please enter the volume for each can (in ounces): ");
+		double canVolume = in.nextDouble();
+	}
+}
